bebef176e1ad5df2929d9ea3f679139ac697dc39
hung.pv <hphamviet83@gmail.com>
2023-06-08 20:44:28 +0700

    fix

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
-export * as storeBaseMap from "@/store/store-baseMap";
-export * as storeMap from "@/store/store-map";
-export * as storeMapLang from "@/store/store-lang";
-export * as storeEvent from "@/store/store-event";
+import * as storeBaseMap from "@/store/store-baseMap";
+import * as storeDatasource from "@/store/store-datasource";
+import * as storeEvent from "@/store/store-event";
+import * as storeMap from "@/store/store-map";
+import * as storeMapLang from "@/store/store-lang";
+export { storeBaseMap, storeDatasource, storeEvent, storeMap, storeMapLang };
