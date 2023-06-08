d17de55b40219eeaa473ed77f7b5bfcfc1a79864
hung.pv <hphamviet83@gmail.com>
2023-06-08 21:07:19 +0700

    fix

---
+++ scripts/build.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-console */
+(async () => {
+  try {
+    const { execa } = await import("execa");
+    await execa(
+      "vue-cli-service build --target lib --name reader build/reader.js",
+      { stdio: "inherit" }
+    );
+    console.log("build reader done");
+    await execa(
+      "vue-cli-service build --target lib --no-clean --name event-bus build/event-bus.js",
+      { stdio: "inherit" }
+    );
+    console.log("build event-bus done");
+    await execa(
+      "vue-cli-service build --target lib --no-clean --name model build/model.js",
+      { stdio: "inherit" }
+    );
+    console.log("build model done");
+    await execa(
+      "vue-cli-service build --target lib --no-clean --name mixin build/mixin.js",
+      { stdio: "inherit" }
+    );
+    console.log("build mixin done");
+    await execa(
+      "vue-cli-service build --target lib --no-clean --name store build/store.js",
+      { stdio: "inherit" }
+    );
+    console.log("build store done");
+    await execa(
+      "vue-cli-service build --target lib --no-clean --name helper build/helper.js",
+      { stdio: "inherit" }
+    );
+    console.log("build helper done");
+    await execa(
+      "vue-cli-service build --target lib --no-clean --name map build/app.js",
+      { stdio: "inherit" }
+    );
+    console.log("build map done");
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e.message);
+    process.exit(1);
+  }
+})();
