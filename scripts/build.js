1772ba30c6009ab146d4af8e8cc7edd43de2449e
hung.pv <hung.pv@eofactory.ai>
2023-06-09 08:58:12 +0700

    fix

--- scripts/build.js
+++ scripts/build.js
@@ -3,37 +3,92 @@
   try {
     const { execa } = await import("execa");
     await execa(
-      "vue-cli-service build --target lib --name reader build/reader.js",
+      "vue-cli-service",
+      ["build", "--target", "lib", "--name", "reader", "build/reader.js"],
       { stdio: "inherit" }
     );
     console.log("build reader done");
     await execa(
-      "vue-cli-service build --target lib --no-clean --name event-bus build/event-bus.js",
+      "vue-cli-service",
+      [
+        "build",
+        "--target",
+        "lib",
+        "--no-clean",
+        "--name",
+        "event-bus",
+        "build/event-bus.js"
+      ],
       { stdio: "inherit" }
     );
     console.log("build event-bus done");
     await execa(
-      "vue-cli-service build --target lib --no-clean --name model build/model.js",
+      "vue-cli-service",
+      [
+        "build",
+        "--target",
+        "lib",
+        "--no-clean",
+        "--name",
+        "model",
+        "build/model.js"
+      ],
       { stdio: "inherit" }
     );
     console.log("build model done");
     await execa(
-      "vue-cli-service build --target lib --no-clean --name mixin build/mixin.js",
+      "vue-cli-service",
+      [
+        "build",
+        "--target",
+        "lib",
+        "--no-clean",
+        "--name",
+        "mixin",
+        "build/mixin.js"
+      ],
       { stdio: "inherit" }
     );
     console.log("build mixin done");
     await execa(
-      "vue-cli-service build --target lib --no-clean --name store build/store.js",
+      "vue-cli-service",
+      [
+        "build",
+        "--target",
+        "lib",
+        "--no-clean",
+        "--name",
+        "store",
+        "build/store.js"
+      ],
       { stdio: "inherit" }
     );
     console.log("build store done");
     await execa(
-      "vue-cli-service build --target lib --no-clean --name helper build/helper.js",
+      "vue-cli-service",
+      [
+        "build",
+        "--target",
+        "lib",
+        "--no-clean",
+        "--name",
+        "helper",
+        "build/helper.js"
+      ],
       { stdio: "inherit" }
     );
     console.log("build helper done");
     await execa(
-      "vue-cli-service build --target lib --no-clean --name map build/app.js",
+      "vue-cli-service",
+      [
+        "build",
+        "--target",
+        "lib",
+        "--no-clean",
+        "--name",
+        "map",
+        "build/app.js"
+      ],
       { stdio: "inherit" }
     );
     console.log("build map done");
